11dfc708292adaa2752aed51dcfc836bbf58a987
Adam <79330948+torontocaper@users.noreply.github.com>
2025-01-30 21:51:02 -0500

    unsuccessfully implemented click-to-move. also trying to create a new branch for movement testing.

---
+++ player_movement/generic_player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+const SPEED : int = 10 ## The player's speed rating
+
+var click_position : Vector2 ## Area where the mouse is clicked, for moving toward
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+		click_position = event.position
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(_delta: float) -> void:
+	velocity = position.direction_to(click_position) * SPEED
+	move_and_slide()
