a0602788084af4575d23c254b01f83790298e5f8
Adam <79330948+torontocaper@users.noreply.github.com>
2025-02-02 16:16:27 -0500

    managed to get tile map coords figued out.

--- player_movement/court_floor_isometric.gd
+++ player_movement/court_floor_isometric.gd
@@ -2,6 +2,17 @@ extends TileMapLayer
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1:
-		print_debug("You clicked on a tile at position " + str(event.position))
-		var grid_position = local_to_map(event.position)
-		print_debug("That translates to " + str(grid_position) + " in the tile grid.")
+		var click_position = event.position
+		print_debug("You clicked on a tile at position " + str(click_position))
+		
+		var local_mouse_position = get_local_mouse_position()
+		print_debug("That's " + str(local_mouse_position) + " in local mouse position.")
+		
+		var map_position = local_to_map(local_mouse_position)
+		print_debug("That translates to " + str(map_position) + " in the tile grid (map position).")
+
+
+func _ready() -> void:
+	var used_tiles = get_used_cells()
+	print_debug(used_tiles)
+	
