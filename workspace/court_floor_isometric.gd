467c1c42126566eeef1ba3692c9a867645463ce0
Adam <79330948+torontocaper@users.noreply.github.com>
2025-02-04 22:03:31 -0500

    added "hovered polygon" (really a polyline)

--- workspace/court_floor_isometric.gd
+++ workspace/court_floor_isometric.gd
@@ -1,19 +1,33 @@
 extends TileMapLayer
 
 signal clicked
+signal hovered
 
-var polygon_points : PackedVector2Array
-@onready var map_position : Vector2
+var clicked_polygon_points : PackedVector2Array
+var map_position : Vector2
+@export var hover_polygon_points : PackedVector2Array
+
+func _process(delta: float) -> void:
+	var hover_position_local := get_local_mouse_position()
+	var hover_position_map := local_to_map(hover_position_local)
+	hover_polygon_points = PackedVector2Array([
+		Vector2(map_to_local(hover_position_map).x + 16, map_to_local(hover_position_map).y),
+		Vector2(map_to_local(hover_position_map).x, map_to_local(hover_position_map).y + 8),
+		Vector2(map_to_local(hover_position_map).x -16, map_to_local(hover_position_map).y),
+		Vector2(map_to_local(hover_position_map).x, map_to_local(hover_position_map).y - 8),
+		Vector2(map_to_local(hover_position_map).x + 16, map_to_local(hover_position_map).y),
+	])
+	hovered.emit(hover_polygon_points)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1:
 		map_position = local_to_map(get_local_mouse_position())
 		print_debug("You clicked on a tile at position " + str(map_position) + " in the tile grid.")
-		polygon_points = PackedVector2Array([
+		clicked_polygon_points = PackedVector2Array([
 			Vector2(map_to_local(map_position).x + 16, map_to_local(map_position).y),
 			Vector2(map_to_local(map_position).x, map_to_local(map_position).y + 8),
 			Vector2(map_to_local(map_position).x -16, map_to_local(map_position).y),
 			Vector2(map_to_local(map_position).x, map_to_local(map_position).y - 8)
 		])
-		print_debug("Drawing polygon with points " + str(polygon_points))
-		clicked.emit(polygon_points)
+		print_debug("Drawing polygon with points " + str(clicked_polygon_points))
+		clicked.emit(clicked_polygon_points)
---
+++ workspace/hovered_polygon.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+var polygon_points : PackedVector2Array
+
+func _draw() -> void:
+	draw_polyline(polygon_points, Color.BLACK, 1.0)
+
+func _on_court_floor_isometric_hovered(hovered_polygon_points : PackedVector2Array) -> void:
+	polygon_points = hovered_polygon_points
+	queue_redraw()
