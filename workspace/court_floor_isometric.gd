4b3b046f7a133104ce00576a2e31962e3913f8a7
Adam <79330948+torontocaper@users.noreply.github.com>
2025-02-05 20:44:47 -0500

    cleaned up a bit of code. going to try something new now.

--- workspace/court_floor_isometric.gd
+++ workspace/court_floor_isometric.gd
@@ -4,12 +4,12 @@ signal clicked
 signal hovered
 
 var clicked_polygon_points : PackedVector2Array
-var map_position : Vector2
+var hover_position_map : Vector2
+var click_position_map : Vector2
 @export var hover_polygon_points : PackedVector2Array
 
 func _process(_delta: float) -> void:
-	var hover_position_local := get_local_mouse_position()
-	var hover_position_map := local_to_map(hover_position_local)
+	hover_position_map = local_to_map(get_local_mouse_position())
 	hover_polygon_points = PackedVector2Array([
 		Vector2(map_to_local(hover_position_map).x + 16, map_to_local(hover_position_map).y),
 		Vector2(map_to_local(hover_position_map).x, map_to_local(hover_position_map).y + 8),
@@ -21,13 +21,13 @@ func _process(_delta: float) -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1:
-		map_position = local_to_map(get_local_mouse_position())
-		print_debug("You clicked on a tile at position " + str(map_position) + " in the tile grid.")
+		click_position_map = local_to_map(get_local_mouse_position())
+		print_debug("You clicked on a tile at position " + str(click_position_map) + " in the tile grid.")
 		clicked_polygon_points = PackedVector2Array([
-			Vector2(map_to_local(map_position).x + 16, map_to_local(map_position).y),
-			Vector2(map_to_local(map_position).x, map_to_local(map_position).y + 8),
-			Vector2(map_to_local(map_position).x -16, map_to_local(map_position).y),
-			Vector2(map_to_local(map_position).x, map_to_local(map_position).y - 8)
+			Vector2(map_to_local(click_position_map).x + 16, map_to_local(click_position_map).y),
+			Vector2(map_to_local(click_position_map).x, map_to_local(click_position_map).y + 8),
+			Vector2(map_to_local(click_position_map).x -16, map_to_local(click_position_map).y),
+			Vector2(map_to_local(click_position_map).x, map_to_local(click_position_map).y - 8)
 		])
 		print_debug("Drawing polygon with points " + str(clicked_polygon_points))
 		clicked.emit(clicked_polygon_points)
