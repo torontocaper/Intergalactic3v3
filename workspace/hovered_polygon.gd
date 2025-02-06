b8dda0537d012d2e9e9cce100f5a8e0c1e05ab9f
Adam <79330948+torontocaper@users.noreply.github.com>
2025-02-05 20:55:04 -0500

    turned hovered polygon into (closed) polyline

--- workspace/hovered_polygon.gd
+++ workspace/hovered_polygon.gd
@@ -1,11 +1,9 @@
-extends Node2D
+extends Line2D
 
-var polygon_points : PackedVector2Array
-
-func _draw() -> void:
-	if polygon_points:
-		draw_polyline(polygon_points, Color.BLACK, 1.0)
+func _ready() -> void:
+	closed = true
+	default_color = Color.BLACK
+	width = 1.0
 
 func _on_court_floor_isometric_hovered(hovered_polygon_points : PackedVector2Array) -> void:
-	polygon_points = hovered_polygon_points
-	queue_redraw()
+	points = hovered_polygon_points
